e5682aa4faa1ead82be4f030a3fb13806eddae53
ryderT <exoryder98@gmail.com>
2020-11-10 14:45:45 +0200

    quickfix

--- finite_automata/FA.py
+++ finite_automata/FA.py
@@ -22,9 +22,29 @@ class FiniteAutomata:
         for line in f:
             chunk1=(line[1],line[4])
             chunk2=line[10]
-            l.append((chunk1,chunk2))  
+            l.append([chunk1,chunk2])  
         return l
+
+    def isDFA(self):
+        transitions=[]
+        for t in self.S:
+            if t[0] in transitions:
+                return False
+            transitions.append(t[0])
+        return True
     
+    def acceptsSequence(self,sequence):
+        current=self.q0
+        trans={}
+        for t in self.S:
+            trans[t[0]]=t[1]
+        for i in sequence:
+            if(current, i) in trans.keys():
+                current = trans[(current,i)]
+            else:
+                return False
+        return current in self.F
+
 
     def __str__(self):
         return "Q="+str(self.Q)+\
@@ -38,25 +58,33 @@ def main():
     #print(str(fa1)) 
     while True:
         print("Commands list:")
-        print("1 - for the list of states(Q)")
-        print("2 - for the alphabet(Sigma)")
-        print("3 - for the initial state(q0)")
-        print("4 - for the list of final states(F)")
-        print("5 - for the list of transitions(S)")
+        print("1 - list of states(Q)")
+        print("2 - alphabet(Sigma)")
+        print("3 - initial state(q0)")
+        print("4 - final states(F)")
+        print("5 - list of transitions(S)")
+        print("6 - isDFA")
+        print("7 - acceptsSequence")
         print("0 - exit")
         cmd = input("Enter your command:")
         if cmd == "0":
             break
         elif cmd == "1":
-            print(fa.Q)
+            print(fa.Q,'\n')
         elif cmd == "2":
-            print(fa.E)
+            print(fa.E,'\n')
         elif cmd == "3":
-            print(fa.q0)
+            print(fa.q0,'\n')
         elif cmd == "4":
-            print(fa.F)
+            print(fa.F,'\n')
+        elif cmd =="6":
+            print(fa.isDFA(),'\n')
+        elif cmd == "7":
+            sequence=input("Enter sequence:")
+            print(fa.acceptsSequence(sequence),'\n')
         elif cmd == "5":
-            print(' '.join([' -> '.join([str(part) for part in trans]) for trans in fa.S]))
+            for i in fa.S:
+                print(i[0],'->',i[1])
         else:
             print("invalid command")
 main()
